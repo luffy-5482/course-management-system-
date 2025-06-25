0b04beabe3a65cab1a99a0cc49191b63bd25f58c
luffy-5482 <tayyabsirkazi@gmail.com>
2025-06-25 15:02:34 +0530

    isolated stroages added

--- admin.java
+++ admin.java
@@ -1,148 +1,131 @@
 import java.util.*;
-class function{
-    Map<String,Integer> student=new HashMap<>();
-    Map<String,Integer> course=new HashMap<>();
-    Map<String,Integer> professor=new HashMap<>();
-    void stud(){
+
+class AdminData {
+    Map<String, Integer> students = new HashMap<>();
+    Map<String, Integer> courses = new HashMap<>();
+    Map<String, Integer> professors = new HashMap<>();
+}
+
+class Function {
+    void func(AdminData data) {
         Scanner sc = new Scanner(System.in);
-        String name;
-        int stdid;
-        System.out.print("enter the student name: ");
-        name=sc.nextLine();
-        System.out.print("enter the student id: ");
-        stdid=sc.nextInt();
-        student.put(name,stdid);
-        System.out.println("student added");
-    }
-    void course(){
-        Scanner sc = new Scanner(System.in);
-        String name;
-        int stdid;
-        System.out.print("enter the course name: ");
-        name=sc.nextLine();
-        System.out.print("enter the course id: ");
-        stdid=sc.nextInt();
-        course.put(name,stdid);
-        System.out.println("course added ");
-    }
-    void prof(){
-        Scanner sc = new Scanner(System.in);
-        String name;
-        int stdid;
-        System.out.print("enter the professor name: ");
-        name=sc.nextLine();
-        System.out.print("enter the professor id: ");
-        stdid=sc.nextInt();
-        professor.put(name,stdid);
-        System.out.println("professor added");
-    }
-    void func(){
-        
-        int n;
-        Scanner sc=new Scanner(System.in);
-        while (true) { 
-            System.out.println("select what operation you want to perform");
-            System.out.println("1.add student 2.add courses 3.add professor 4.display students 5.display course 6.display professor 7.exit");
-            n=sc.nextInt();
-            switch(n){
+        while (true) {
+            System.out.println("Select the operation you want to perform:");
+            System.out.println("1. Add student  2. Add course  3. Add professor");
+            System.out.println("4. Display students  5. Display courses  6. Display professors  7. Exit");
+            int n = sc.nextInt();
+            sc.nextLine();
+
+            switch (n) {
                 case 1:
-                    stud();
+                    System.out.print("Enter the student name: ");
+                    String studentName = sc.nextLine();
+                    System.out.print("Enter the student ID: ");
+                    int studentId = sc.nextInt();
+                    data.students.put(studentName, studentId);
+                    System.out.println("Student added.");
                     break;
                 case 2:
-                    course();
+                    System.out.print("Enter the course name: ");
+                    String courseName = sc.nextLine();
+                    System.out.print("Enter the course ID: ");
+                    int courseId = sc.nextInt();
+                    data.courses.put(courseName, courseId);
+                    System.out.println("Course added.");
                     break;
                 case 3:
-                    prof();
+                    System.out.print("Enter the professor name: ");
+                    String professorName = sc.nextLine();
+                    System.out.print("Enter the professor ID: ");
+                    int professorId = sc.nextInt();
+                    data.professors.put(professorName, professorId);
+                    System.out.println("Professor added.");
                     break;
                 case 4:
-                    {
-                        for(HashMap.Entry<String,Integer> stud : student.entrySet())
-                             {
-                                System.out.println("student name:"+stud.getKey()+"student id"+stud.getValue());
-                             }
+                    for (Map.Entry<String, Integer> entry : data.students.entrySet()) {
+                        System.out.println("Student Name: " + entry.getKey() + "  Student ID: " + entry.getValue());
                     }
                     break;
                 case 5:
-                    {
-                        for(HashMap.Entry<String,Integer> crs : course.entrySet())
-                             {
-                                System.out.println("student name:"+crs.getKey()+"student id"+crs.getValue());
-                             }
+                    for (Map.Entry<String, Integer> entry : data.courses.entrySet()) {
+                        System.out.println("Course Name: " + entry.getKey() + "  Course ID: " + entry.getValue());
                     }
                     break;
-                case 6:
-                    {
-                        for(HashMap.Entry<String,Integer> prf : professor.entrySet())
-                             {
-                                System.out.println("student name:"+prf.getKey()+"student id"+prf.getValue());
-                             }
+                case 6: 
+                    for (Map.Entry<String, Integer> entry : data.professors.entrySet()) {
+                        System.out.println("Professor Name: " + entry.getKey() + "  Professor ID: " + entry.getValue());
                     }
                     break;
+                case 7: 
+                    return;
                 default:
-                System.out.println("enter the correct choice");
+                    System.out.println("Enter the correct choice.");
             }
         }
     }
-        
 }
-class login extends function{
-     Map<String,Integer> users=new HashMap<>();
-    void  log(){
-         Scanner sc=new Scanner(System.in);
-        
-        System.out.println("create username and password");
-        System.out.println("make sure that password is only of numeric numbers");
-        System.out.print("enter the username: ");
-        String user=sc.nextLine();
-        
-        if(users.containsKey(user))
-        {
-            System.out.println("the username already exists try different username");
+
+class Login extends Function {
+    Map<String, Integer> users = new HashMap<>(); 
+    Map<String, AdminData> adminStorage = new HashMap<>();
+
+    void log() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Create username and password");
+        System.out.println("Make sure that password is only numeric numbers.");
+        System.out.print("Enter the username: ");
+        String user = sc.nextLine();
+
+        if (users.containsKey(user)) {
+            System.out.println("The username already exists. Try a different username.");
             return;
         }
-        System.out.print("enter the password: ");
-        int pass=sc.nextInt();
-        
-            users.put(user,pass);  
+        System.out.print("Enter the password: ");
+        int pass = sc.nextInt();
+        users.put(user, pass);
+        adminStorage.put(user, new AdminData());
+        System.out.println("Account created successfully.");
+    }
+    void signin() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the username: ");
+        String user = sc.nextLine();
+        System.out.print("Enter the password: ");
+        int pass = sc.nextInt();
 
-            System.out.println("account created successfully");
-    }
-    void signin()
-    {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("enter the username: ");
-        String user=sc.nextLine();
-        System.out.print("enter the password: ");
-        int pass=sc.nextInt();
-        if(users.containsKey(user) && users.containsValue(pass))
-        {
+        if (users.containsKey(user) && users.get(user) == pass) {
             System.out.println("----LOGGED IN SUCCESSFULLY----");
-            func();
-        }
-        else{
-            System.out.println("username or password is wrong");
+            func(adminStorage.get(user)); 
+        } else {
+            System.out.println("Username or password is wrong.");
         }
     }
 }
-public class admin{
+public class admin {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        login lobj=new login();
-        while(true){
-        System.out.println("1.create an account "+"\n"+"2.already have an account"+"\n"+"3.exit");
-        int n=sc.nextInt();
-        switch (n) {
-            case 1:
-                lobj.log();
-                break;
-            case 2:
-                lobj.signin();
-                 break;
-            case 3:
-                return;
-            default:
-                System.out.println("choose the correct option");
+        Scanner sc = new Scanner(System.in);
+        Login lobj = new Login();
+
+        while (true) {
+            System.out.println("1. Create an account");
+            System.out.println("2. Already have an account");
+            System.out.println("3. Exit");
+            int n = sc.nextInt();
+            sc.nextLine();
+
+            switch (n) {
+                case 1:
+                    lobj.log();
+                    break;
+                case 2:
+                    lobj.signin();
+                    break;
+                case 3:
+                    System.out.println("Exiting the system. Goodbye!");
+                    return;
+                default:
+                    System.out.println("Choose the correct option.");
+            }
         }
     }
-}
 }
