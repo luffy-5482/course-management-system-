d2763c19485dffe512c1f23448e28528a05de275
luffy-5482 <tayyabsirkazi@gmail.com>
2025-06-25 10:53:29 +0530

    signin added

--- admin.java
+++ admin.java
@@ -14,21 +14,34 @@ class login{
             System.out.println("the username already exists try different username");
             return;
         }
-        System.out.print("enter the password ");
+        System.out.print("enter the password:");
         int pass=sc.nextInt();
         
             users.put(user,pass);  
+
+            System.out.println("account created successfully");
     }
     void signin()
     {
         Scanner sc=new Scanner(System.in);
-        int n;
+        System.out.print("enter the username: ");
+        String user=sc.nextLine();
+        System.out.print("enter the password: ");
+        int pass=sc.nextInt();
+        if(users.containsKey(user) && users.containsValue(pass))
+        {
+            System.out.println("----LOGGED IN SUCCESSFULLY----");
+        }
+        else{
+            System.out.println("username or password is wrong");
+        }
     }
 }
 public class admin{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         login lobj=new login();
+        while(true){
         System.out.println("1.create an account 2.already have an account");
         int n=sc.nextInt();
         switch (n) {
@@ -36,8 +49,12 @@ public class admin{
                 lobj.log();
                 
                 break;
+            case 2:
+            lobj.signin();
+            break;
             default:
                 System.out.println("choose the correct option");
         }
     }
+}
 }
