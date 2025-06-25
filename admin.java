3e77ff313329544e43fd000475d41c2dcab0071f
luffy-5482 <tayyabsirkazi@gmail.com>
2025-06-25 12:53:17 +0530

    some modification

--- admin.java
+++ admin.java
@@ -1,5 +1,91 @@
 import java.util.*;
-class login{
+class function{
+    Map<String,Integer> student=new HashMap<>();
+    Map<String,Integer> course=new HashMap<>();
+    Map<String,Integer> professor=new HashMap<>();
+    void stud(){
+        Scanner sc = new Scanner(System.in);
+        String name;
+        int stdid;
+        System.out.print("enter the student name: ");
+        name=sc.nextLine();
+        System.out.print("enter the student id: ");
+        stdid=sc.nextInt();
+        student.put(name,stdid);
+        System.out.println("student added");
+    }
+    void course(){
+        Scanner sc = new Scanner(System.in);
+        String name;
+        int stdid;
+        System.out.print("enter the course name: ");
+        name=sc.nextLine();
+        System.out.print("enter the course id: ");
+        stdid=sc.nextInt();
+        course.put(name,stdid);
+        System.out.println("course added ");
+    }
+    void prof(){
+        Scanner sc = new Scanner(System.in);
+        String name;
+        int stdid;
+        System.out.print("enter the professor name: ");
+        name=sc.nextLine();
+        System.out.print("enter the professor id: ");
+        stdid=sc.nextInt();
+        professor.put(name,stdid);
+        System.out.println("professor added");
+    }
+    void func(){
+        
+        int n;
+        Scanner sc=new Scanner(System.in);
+        while (true) { 
+            System.out.println("select what operation you want to perform");
+            System.out.println("1.add student 2.add courses 3.add professor 4.display students 5.display course 6.display professor 7.exit");
+            n=sc.nextInt();
+            switch(n){
+                case 1:
+                    stud();
+                    break;
+                case 2:
+                    course();
+                    break;
+                case 3:
+                    prof();
+                    break;
+                case 4:
+                    {
+                        for(HashMap.Entry<String,Integer> stud : student.entrySet())
+                             {
+                                System.out.println("student name:"+stud.getKey()+"student id"+stud.getValue());
+                             }
+                    }
+                    break;
+                case 5:
+                    {
+                        for(HashMap.Entry<String,Integer> crs : course.entrySet())
+                             {
+                                System.out.println("student name:"+crs.getKey()+"student id"+crs.getValue());
+                             }
+                    }
+                    break;
+                case 6:
+                    {
+                        for(HashMap.Entry<String,Integer> prf : professor.entrySet())
+                             {
+                                System.out.println("student name:"+prf.getKey()+"student id"+prf.getValue());
+                             }
+                    }
+                    break;
+                default:
+                System.out.println("enter the correct choice");
+            }
+        }
+    }
+        
+}
+class login extends function{
      Map<String,Integer> users=new HashMap<>();
     void  log(){
          Scanner sc=new Scanner(System.in);
@@ -14,7 +100,7 @@ class login{
             System.out.println("the username already exists try different username");
             return;
         }
-        System.out.print("enter the password:");
+        System.out.print("enter the password: ");
         int pass=sc.nextInt();
         
             users.put(user,pass);  
@@ -31,6 +117,7 @@ class login{
         if(users.containsKey(user) && users.containsValue(pass))
         {
             System.out.println("----LOGGED IN SUCCESSFULLY----");
+            func();
         }
         else{
             System.out.println("username or password is wrong");
@@ -42,16 +129,17 @@ public class admin{
         Scanner sc=new Scanner(System.in);
         login lobj=new login();
         while(true){
-        System.out.println("1.create an account 2.already have an account");
+        System.out.println("1.create an account "+"\n"+"2.already have an account"+"\n"+"3.exit");
         int n=sc.nextInt();
         switch (n) {
             case 1:
                 lobj.log();
-                
                 break;
             case 2:
-            lobj.signin();
-            break;
+                lobj.signin();
+                 break;
+            case 3:
+                return;
             default:
                 System.out.println("choose the correct option");
         }
